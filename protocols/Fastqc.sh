0c583fb6a6882fc421e9521252b0d8117d1b3e58
mmterpstra <m.m.terpstra.cluster@gmail.com>
2016-08-12 16:42:12 +0200

    testing if this will break travis:Correcting the code

--- protocols/Fastqc.sh
+++ protocols/Fastqc.sh
@@ -18,7 +18,6 @@
 #string singleEndfastqcZip
 #string pairedEndfastqcZip1
 #string pairedEndfastqcZip2
-#string WillThisBreakCI
 echo -e "test ${reads1FqGz} ${reads2FqGz} 1: $(basename ${reads1FqGz} .gz)${fastqcZipExt} \n2: $(basename ${reads2FqGz} .gz)${fastqcZipExt} "
 
 ${stage} ${fastqcMod}
