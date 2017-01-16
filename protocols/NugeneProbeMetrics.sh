03839f49cc3cc18846eb84d596ad5c19a42b8db0
mmterpstra <m.m.terpstra.cluster@gmail.com>
2017-01-16 16:02:34 +0100

    - update NugeneProbeMetrics.sh to output counts without duplicates.

--- protocols/NugeneProbeMetrics.sh
+++ protocols/NugeneProbeMetrics.sh
@@ -7,6 +7,7 @@
 #string stage
 #string checkStage
 #string bedtoolsMod
+#string samtoolsMod
 
 #string probeBed
 #string onekgGenomeFasta
@@ -15,11 +16,11 @@
 #string indelRealignmentBai
 #string nugeneProbeMetricsDir
 #string nugeneProbeMetricsLog
-
+#string nugeneProbeMetricsDeDupLog
 echo "## "$(date)" ##  $0 Started "
 
 alloutputsexist \
- ${nugeneProbeMetricsLog} 
+ ${nugeneProbeMetricsLog} ${nugeneProbeMetricsDeDupLog}
 
 getFile ${onekgGenomeFasta}
 getFile ${probeBed}
@@ -27,6 +28,7 @@ getFile ${indelRealignmentBam}
 getFile ${indelRealignmentBai}
 
 ${stage} ${bedtoolsMod}
+${stage} ${samtoolsMod}
 ${checkStage}
 
 set -x
@@ -61,6 +63,32 @@ cat - \
 sort | \
 uniq -c  > ${nugeneProbeMetricsLog}
 
-putFile ${nugeneProbeMetricsLog}
+bedtools closest -D 'a' -fd -iu -S -a \
+        <(bedtools sort -i \
+                <(bedtools bamtobed  -i \
+			<(samtools view -hb -F 1024 ${indelRealignmentBam})))\
+ -b  \
+      	<(bedtools sort -i \
+                <(bedtools slop \
+                 -l 60 \
+                 -r -60 \
+                 -s \
+                 -i ${probeBed} \
+                 -g <(cut -f1,2 ${onekgGenomeFasta}.fai))) | \
+awk '{if($13 < 600 &&  $10 != "." ){print $0}}' | \
+cut -f7,8,9,10,11,12| sort | \
+cat - \
+       	<(bedtools sort -i \
+                <(bedtools slop \
+                 -l 60 \
+                 -r -60 \
+                 -s \
+                 -i ${probeBed} \
+                 -g <(cut -f1,2 ${onekgGenomeFasta}.fai))) | \
+sort | \
+uniq -c  > ${nugeneProbeMetricsDeDupLog}
+
+putFile ${nugeneProbeMetricsLog} 
+putFile ${nugeneProbeMetricsDeDupLog}
 
 echo "## "$(date)" ##  $0 Done "
