2adeecac7b5c35601e7d4c60be24d4dbb709d2f5
mmterpstra <m.m.terpstra.cluster@gmail.com>
2017-05-30 14:08:33 +0200

    Update tests by deleting files and expanding current tests

--- tests/weave_test.sh
+++ tests/weave_test.sh
@@ -4,3 +4,7 @@ set -x
 
 touch dummy.interval_list && bash ../GenerateScripts2.sh $1 ../samplesheet.csv exome dummy.interval_list
 bash .RunWorkFlowGeneration.sh
+for i in  /home/travis/projects/exome/jobs/*.sh; do
+	>&2 echo  "## "$(date)" ## $0 ## checking $i"
+	bash -n "$i"
+done
