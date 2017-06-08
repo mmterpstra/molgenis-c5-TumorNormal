576e8d0d8f4241eb336b057fc5264afab3abc814
mmterpstra <m.m.terpstra.cluster@gmail.com>
2017-06-08 15:46:31 +0200

    fix travis test

--- tests/weave_test.sh
+++ tests/weave_test.sh
@@ -2,7 +2,14 @@
 set -e
 set -x
 
-touch dummy.interval_list && bash ../GenerateScripts2.sh $1 ../samplesheet.csv exome dummy.interval_list
+probe=""
+
+if [ $1 == "nugene" ] ; then
+	probe="data/resource/probe.bed"
+elif [ $1 == "nugrna" ] ; then
+	probe="data/resource/probe.bed"
+fi
+touch dummy.interval_list && bash ../GenerateScripts2.sh $1 ../samplesheet.csv exome dummy.interval_list $probe
 bash .RunWorkFlowGeneration.sh
 for i in  /home/travis/projects/exome/jobs/*.sh; do
 	>&2 echo  "## "$(date)" ## $0 ## checking $i"
