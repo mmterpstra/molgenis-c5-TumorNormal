9e3f9d68dc67e9a45c36cfc2924a5aa817a5cb45
mmterpstra <m.m.terpstra.cluster@gmail.com>
2017-06-14 13:41:18 +0200

    Added MultiQC to nugene workflows

---
+++ protocols/MultiQC.sh
@@ -0,0 +1,33 @@
+#MOLGENIS walltime=23:59:00 mem=4gb nodes=1 ppn=4
+
+#string project
+#string projectDir
+
+#string multiQcMod
+#string stage
+#string checkStage
+
+
+#string projectMultiQcHtml
+
+alloutputsexist \
+ ${projectMultiQcHtml} \
+
+echo "## "$(date)" Start $0"
+
+#load modules
+${stage} ${multiqcMod}
+
+${checkStage}
+
+set -x
+set -e
+
+
+cd ${projectDir}
+multiqc ./
+
+putFile ${projectMultiQcHtml}
+
+echo "## "$(date)" Done $0"
+
