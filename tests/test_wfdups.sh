99ada83b8405b3867dbbb476a48ea589d90895ff
mmterpstra <m.m.terpstra.cluster@gmail.com>
2017-10-19 09:36:18 +0200

    Update test for workflow duplications. Update failing workflow.

--- tests/test_wfdups.sh
+++ tests/test_wfdups.sh
@@ -1,4 +1,12 @@
 set -e;
 set -x;
-for wf in $(ls ../workflow*.csv); do if [ $(grep -v '^#' $wf |sort -t, | wc -l) -eq $(grep -v '^#' $wf |sort -k1,1 -u -t, | wc -l) ]; then echo "No dups in "$wf;else echo "Dups in $wf file detected. Conflicting record shown below.";  diff <(grep -v '^#' $wf |sort -t,) <(grep -v '^#' $wf |sort -k1,1 -u -t,); exit 1; fi; done
+for wf in $(ls ../workflow*.csv); do 
+	if [ $(grep -v '^#' $wf |sort -t, | wc -l) -eq $(grep -v '^#' $wf |sort -k1,1 -u -t, | wc -l) ]; then 
+		echo "## INFO ## No dups in "$wf;
+	else 
+		echo "## ERROR ## Dups in $wf file detected. Conflicting record shown below."; 
+		diff <(grep -v '^#' $wf |sort -t,) <(grep -v '^#' $wf |sort -k1,1 -u -t,)| perl -wpe 's/^/## ERROR ##/g'; 
+		exit 1;
+	fi;
+done
 
